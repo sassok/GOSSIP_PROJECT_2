2f05243f2acadc7e5a95267b19498e58affa27a2
anthopayan <anthony.payan08@outlook.fr>
2021-02-10 17:29:36 +0100

    work17:29

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,5 +1,6 @@
 class Gossip < ApplicationRecord
     belongs_to :user 
+    has_many :cities, through: :users
     validates :title, length: { in: 3..14 }, presence: true
     validates :content, presence: true
 end
