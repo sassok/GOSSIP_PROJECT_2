23b39957042a01ca8d3a0882c5b5b0fa6eae26f9
sassok <kevin.sassoo@gmail.com>
2021-02-10 14:00:55 +0100

    User city

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :gossips
-    
+    belongs_to :city
     validates :last_name, length: { minimum: 2 }, presence: true
     validates :first_name, length: { minimum: 2 }, presence: true
     validates :age, presence: true
