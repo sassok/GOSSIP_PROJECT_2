4d51049d6aee6c1393261e01d1edbcc46e43114f
anthopayan <anthony.payan08@outlook.fr>
2021-02-10 11:24:10 +0100

    Migrate3

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,20 @@
+class GossipsController < ApplicationController
+  def new
+  end
+  def create
+    @gossip = Gossip.new(title: params["title"], content: params["content"], user_id: 1) # avec xxx qui sont les données obtenues à partir du formulaire
+  
+    if @gossip.save # essaie de sauvegarder en base @gossip
+      # si ça marche, il redirige vers la page d'index du site
+      redirect_to home_path
+      flash.notice = "Gossip bien enregistré"
+
+      
+    else
+      # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+      render_to new_gossip_path
+      flash.alert = "Try again, le Gossip n'est pas complet !!!"
+    end
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :gossips, only: [:new, :create]
+  get 'user/:id', to: 'user#show'
+  get 'gossips/:id', to: 'gossips#show'
+  get '/home/', to: 'home#home'
+  get 'welcome/:name', to: 'welcome#show'
+  get 'static_pages/contact', to: 'static_pages#contact'
+  get 'static_pages/team', to: 'static_pages#team'
+  #For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
